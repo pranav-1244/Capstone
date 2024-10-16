446ab7c937dfe0b3032a71d332cd4b8571552633
pranav-1244 <pprv.41@gmail.com>
2024-10-16 18:01:59 +0530

    Add files via upload

---
+++ Guvi-capstone/src/main/java/com/example/Guvi_capstone/GuviCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.Guvi_capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuviCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuviCapstoneApplication.class, args);
+	}
+
+}
---
+++ Guvi-capstone/src/test/java/com/example/Guvi_capstone/GuviCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Guvi_capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuviCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
